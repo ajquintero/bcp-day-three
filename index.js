1b1a1d07d0beb71a1d034eb10fd8ab7dcf6b42bb
Abel Quintero <abel.j.quintero@gmail.com>
2018-10-03 20:26:45 -0700

    added even counter for loop

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("Hello bcpreppers!");
+var x = 11;
+
+if(x % 2 === 0) {
+   console.log("is even");
+}
+
+console.log("all done");
+
+// (init; condition; post-loop)
+for(i = 0; i < 100; i++){
+    if(i % 2 == 0){
+        console.log(i + "is an even number");
+    } else {
+        (console.log(i + " is an odd number"));
+    }
+}
